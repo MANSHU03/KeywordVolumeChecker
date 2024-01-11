1e7eab3c9c8af38d63e16d312b32d65c4163a95e
Manshu Jaiswar <manshu.jaiswar.cd.mec20@itbhu.ac.in>
2024-01-11 18:35:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express= require("express");
+const bodyparser=require("body-parser");
+const mongoose=require("mongoose");
+const dotenv= require("dotenv")
+const app=express();
+app.set('view engine','ejs');
+dotenv.config();
+let keyw="";
+let freq;
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",(req,res)=>{
+    
+    res.render(__dirname+'/index',{key:keyw,freq:freq})
+});
+const Schema=mongoose.Schema;
+const keyschema=new Schema({keyword: String , frequency: Number});
+const key=mongoose.model("key",keyschema);
+
+app.post("/",(req,res)=>{
+    keyw=req.body.item;
+    
+   key.findOne({keyword: keyw}).then(function(data){
+
+    if(data){
+        const id=data._id;
+        const f=data.frequency
+       // console.log(id);
+         key.findByIdAndUpdate(id,{frequency:f+1}).then((key)=>{
+           //    console.log(key);
+               freq= f+1;
+               res.redirect("/");
+        })
+    
+    }
+    else{
+        key.create({keyword: keyw, frequency: 1}).then((user)=>{console.log(key); freq=1; res.redirect("/");} );
+        
+    }
+   })
+   
+   // console.log(keyw);
+   // res.redirect("/");
+});
+
+
+ mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+     .then(()=> app.listen(process.env.PORT, () => console.log(`server is running on ${process.env.PORT} and key=${keyw}`)))
+     .catch((error) => console.log(error.message));
+//app.listen(PORT,()=>console.log(`server is running on ${PORT} and key=${keyw}`));
